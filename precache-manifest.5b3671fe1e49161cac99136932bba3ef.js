0a14dbf97339b2f8775aab153b8cecb6373ddb19
Yvette Willems <yvetje.willems@gmail.com>
2019-05-14 10:33:53 +0200

    first commit

---
+++ precache-manifest.5b3671fe1e49161cac99136932bba3ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8baff076bc2447c1275d4b4cfdf1e1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cc5db97294813b25a34",
+    "url": "/static/css/main.d2cc8e1f.chunk.css"
+  },
+  {
+    "revision": "f505be76876dd6e952d1",
+    "url": "/static/js/2.b8280c10.chunk.js"
+  },
+  {
+    "revision": "5cc5db97294813b25a34",
+    "url": "/static/js/main.73ef4be7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
